d95b7999a34815c39eb67c92503baabb5105d0cd
Jérémie Drouet <jeremie.drouet@gmail.com>
2021-10-28 20:28:14 +0200

    cleanup testing script
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- testing.sh
+++ testing.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
-set -x
+# set -x
+
+PORT=3000
 
 function cleanup() {
 	echo "# doing some cleanup..."
-	docker rm -f $(docker ps -aq)
+	docker rm -f $(docker ps -aq) &> /dev/null
 }
 
 function wait_for_server() {
-	echo "# waiting for server on port $1"
-	curl http://localhost:$1/
-	if [ $? -eq 0 ]; then
-		echo "# server seems to be ready..."
-	else
-		sleep 1
-		wait_for_server $1
-	fi
+	echo "# waiting for server"
+	start_date=$(date +"%s")
+	while :; do
+		curl http://localhost:$PORT/ &> /dev/null
+		if [ $? -eq 0 ]; then
+			break
+		fi
+	done
+	end_date=$(date +"%s")
+	diff_date=$(($end_date-$start_date))
+	echo "# server $1 took $diff_date seconds to start" 
+	echo "boot,$1,$diff_date" >> timing.csv
 }
 
 function test_server() {
@@ -24,24 +30,25 @@ function test_server() {
 	sleep 5
 	echo "# build and start the server..."
 	docker-compose up --build -d $1
-	wait_for_server $2
+	wait_for_server $1
 	echo "# publishing some logs..."
 	for i in {0..10}; do
 		createdAt=$(echo $i + 1633462963 | bc)
 		curl -X POST -H "Content-type: Application/json" \
 			--data "[{\"createdAt\":$createdAt,\"level\":\"info\",\"message\":\"hello world\",\"index\":$i}]" \
-			http://localhost:$2/publish
+			http://localhost:$PORT/publish
 	done
 	echo "# searching for logs..."
-	curl http://localhost:$2/search
+	curl http://localhost:$PORT/search &> /dev/null
+	echo
 	cleanup
 }
 
 cleanup
-test_server "golang" 8080
-test_server "java" 8080
-test_server "nodejs" 3000
-test_server "php" 8000
-test_server "python" 5000
-test_server "rust" 3000
+test_server "golang"
+test_server "java"
+test_server "nodejs"
+test_server "php"
+test_server "python"
+test_server "rust"
 
